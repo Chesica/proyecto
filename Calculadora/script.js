ef6433f6ec4c7ac5dd59cd11d59187a0952b7aa6
Chesica <159648809+Chesica@users.noreply.github.com>
2024-02-10 16:04:55 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value +=value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try  {
+        document.getElementById('display').value =
+        eval(document.getElementById('display').value);
+        } catch(error) {
+            document.getElementById('display').value = 'Error';
+
+           }
+        }
+
+
